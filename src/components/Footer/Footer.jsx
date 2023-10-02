a8f1618ebee2e5c2b28aafee2911f69fc5f2af42
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-10-02 20:41:36 +0300

    add base markup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,82 @@
+import Buttons from "../Buttons/Buttons";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>
+        <a href="/">logo</a>
+        <form action="">
+          <input type="email" />
+          <Buttons>Send</Buttons>
+        </form>
+      </div>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About us</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li>
+          <a href="">Pricing</a>
+        </li>
+        <li>
+          <a href="">FAQ</a>
+        </li>
+        <li>
+          <a href="">Contacts</a>
+        </li>
+      </ul>
+      <p>Help</p>
+      <ul>
+        <li>
+          <a href="">Support</a>
+        </li>
+        <li>
+          <a href="">Partner Help Center</a>
+        </li>
+        <li>
+          <a href="">Dispute Resolution</a>
+        </li>
+        <li>
+          <a href="">Terms and Conditions</a>
+        </li>
+      </ul>
+      <div>
+        <b>Do you have a question?</b>
+        <p>
+          Write to us at the email address:
+          <a href="companyname@gmail.com">companyname@gmail.com</a>
+        </p>
+        <p>
+          Phone:
+          <a href="tel:+48 000 000 000">+48 000 000 000</a>
+        </p>
+      </div>
+      <div>
+        <p>Social networks</p>
+        <ul>
+          <li>
+            <a href=""></a>icon
+          </li>
+          <li>
+            <a href=""></a>icon
+          </li>
+          <li>
+            <a href=""></a>icon
+          </li>
+          <li>
+            <a href=""></a>icon
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>Company name</p>
+        <p>All rights reserved Copyright (2006 - 2023) - Name™</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,37 @@
+import Link from "next/link";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <a href="/">Logo svg or img</a>
+      <ul>
+        <li>
+          <a id="#about" href="">
+            About us
+          </a>
+        </li>
+        <li>
+          <a id="#services" href="">
+            Services
+          </a>
+        </li>
+        <li>
+          <a id="#pricing" href="">
+            Pricing
+          </a>
+        </li>
+        <li>
+          <a id="#faq" href="">
+            FAQ
+          </a>
+        </li>
+        <li>
+          <a id="#contacts" href="">
+            Contacts us
+          </a>
+        </li>
+      </ul>
+      <Link href="/login">Login</Link>
+    </header>
+  );
 }
