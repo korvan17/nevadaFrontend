b9f3e637d828ed549076af2a8faf8e56f70e9128
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-10-06 22:35:17 +0300

    fix desk header styles

--- src/components/Header/MobileHeader.jsx
+++ src/components/Header/MobileHeader.jsx
@@ -12,11 +12,14 @@ function MobileHeader() {
   };
 
   return (
-    <header className="lg:hidden flex w-[100%] fixed top-0 bg-mainBlack text-mainWhite pt-[39px] pb-[8px] px-[16px]">
+    <header className="lg:hidden md:pt-[38px] md:pb-[8px] md:pl-[16px] md:pr-[40px] flex w-[100%] fixed top-0 bg-mainBlack text-mainWhite pt-[39px] pb-[8px] px-[16px]">
+      <div className="hidden left-0 md:flex lg:hidden text-[16px] font-semibold	 w-[100%] absolute top-0 h-[30px] items-center justify-center bg-captionBlue text-captionalWhite">
+        <a href="tel:+48000000000">+48 000 000 000</a>
+      </div>
       <a className="cursor-pointer flex gap-[8px] items-center" href="#home">
         <Logo />
         <div>
-          <p className="text-[18px] font-bold text-mainWhite leading-[20px]">
+          <p className="text-[18px] md:text-[24px] font-bold text-mainWhite leading-[20px]">
             Prime Preparation Center
           </p>
         </div>
@@ -25,9 +28,9 @@ function MobileHeader() {
         <BurgerMenuBtn />
       </button>
       {isMenuOpen && (
-        <div className="flex justify-center items-center flex-col gap-[48px] bg-captionBlue pt-[86px] px-[8px] w-[100%] h-[100%] fixed top-0 left-0 text-mainWhite">
+        <div className="flex justify-start items-center md:items-start flex-col gap-[48px] bg-captionBlue pt-[86px] px-[8px]  md:pt-[110px] md:px-[12px]  w-[100%] h-[100%] fixed top-0 right-0 text-mainWhite">
           <button
-            className="absolute top-[79px] left-0 flex justify-center items-center w-[24px] h-[24px]"
+            className="absolute top-[38px] left-0 md:left-[24px] md:top-[62px] flex justify-center items-center w-[24px] h-[24px]"
             onClick={handleOpenMenu}
             type="button"
           >
@@ -35,11 +38,19 @@ function MobileHeader() {
           </button>
           <UserControls />
           <div className="text-mainWhite text-left ">
-            <h3 className="mb-[12px]">Contacts</h3>
-            <a className="block mb-[8px]" href="tel:+48000000000">
+            <h3 className="mb-[12px] text-[16px] md:text-[24px]">Contacts</h3>
+            <a
+              className="block mb-[8px] text-[14px] md:text-[16px]"
+              href="tel:+48000000000"
+            >
               +48 000 000 000
             </a>
-            <a href="mailto:ppc.company@gmail.com">ppc.company@gmail.com</a>
+            <a
+              className="text-[14px] md:text-[16px]"
+              href="mailto:ppc.company@gmail.com"
+            >
+              ppc.company@gmail.com
+            </a>
           </div>
         </div>
       )}
--- src/components/UserControls/UserControls.jsx
+++ src/components/UserControls/UserControls.jsx
@@ -5,12 +5,15 @@ const lang = ["EN", "UA", "RU"];
 
 function UserControls() {
   return (
-    <div className="flex flex-wrap flex-col lg:flex-row lg:flex-nowrap justify-center items-center gap-[18px]">
-      <ul className="flex">
+    <div className="flex flex-wrap flex-col lg:flex-row lg:flex-nowrap justify-center  gap-[8px] lg:gap-[18px] md:items-start lg:items-center">
+      <ul className="flex ">
         {lang.map((e) => (
-          <li key={e}>
+          <li
+            className="last:border-r-0 border-b border-r border-r-captionalGreyLight border-b-captionalGreyLight lg:border-b-captionalGrey lg:border-r-captionalGrey transition-all ease-out hover:border-b-mainWhite hover:border-r-mainWhite "
+            key={e}
+          >
             <button
-              className="lg:text-captionalGrey font-medium flex items-center h-[16px] px-[4px] border-b  border-r border-r-mainWhite border-b-captionalGreyLight lg:border-b-captionalGrey   transition-all ease-out cursor-pointer text-captionalGreyLight hover:text-mainWhite active:text-mainWhite hover:border-b-mainWhite"
+              className="lg:text-captionalGrey font-medium text-[16px] flex items-center h-[16px] px-[4px]     transition-all ease-out cursor-pointer text-captionalGreyLight hover:text-mainWhite active:text-mainWhite "
               type="button"
             >
               {e}
@@ -18,8 +21,8 @@ function UserControls() {
           </li>
         ))}
       </ul>
-      <div className="flex flex-wrap flex-col lg:flex-row lg:flex-nowrap gap-[8px] items-center">
-        <button className="lg:text-captionalGrey text-captionalGreyLight font-semibold	 hover:text-mainWhite transition-all ease-out cursor-pointer">
+      <div className="flex flex-wrap flex-col lg:flex-row lg:flex-nowrap lg:items-center gap-[8px] items-center md:items-start">
+        <button className="lg:text-captionalGrey text-[16px] text-captionalGreyLight font-semibold	 hover:text-mainWhite transition-all ease-out cursor-pointer">
           Registration
         </button>
         <Buttons login={true}>Login</Buttons>
