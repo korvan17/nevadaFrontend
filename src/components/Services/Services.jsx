d9e410df3afa697e141695103205963ded009ba5
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-10-09 13:39:48 +0200

    completed desktop markup

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,40 +1,41 @@
 import Container from "@/components/Container/Container";
 import Section from "../Section/Section";
+import Image from "next/image";
 
 export default function Services() {
   return (
     <Section sectionName={"services"} id="#services">
       <Container>
-        <h2>Explore Our Offerings</h2>
-        <ul>
-          <li>
-            {/* <Image src="" alt="icon" /> */}
-            <h3>Product Procurement</h3>
-            <p>
+        <h2 className="mb-[48px] text-5xl font-bold">Our Services</h2>
+        <ul className="flex">
+          <li className="mr-[24px] pt-[20px] pb-[24px] px-[20px] rounded-[12px] bg-darkBlue text-captionalWhite text-center">
+            <Image src="/procurement-desktop-1x.png" width={234} height={234} alt="icon" />
+            <h3 className="mt-[24px] mb-[12px] text-2xl font-bold">Product Procurement</h3>
+            <p className="text-base">
               We simplify Amazon product ordering with seamless transactions and
               reliable delivery
             </p>
           </li>
-          <li>
-            {/* <Image src="" alt="icon" /> */}
-            <h3>Global Accessibility</h3>
-            <p>
+          <li className="mr-[24px] pt-[20px] pb-[24px] px-[24px] rounded-[12px] bg-darkBlue text-captionalWhite text-center">
+          <Image src="/accessibility-desktop-1x.png" width={234} height={234} alt="icon" />
+            <h3 className="mt-[24px] mb-[12px] text-2xl font-bold">Global <br></br> Accessibility</h3>
+            <p className="text-base">
               Access Amazon`s extensive product range, regardless of your
               location or business size
             </p>
           </li>
-          <li>
-            {/* <Image src="" alt="icon" /> */}
-            <h3>Efficient Shipping</h3>
-            <p>
+          <li className="mr-[24px] pt-[20px] pb-[24px] px-[24px] rounded-[12px] bg-darkBlue text-captionalWhite text-center">
+          <Image src="/delivery-desktop-1x.png" width={234} height={234} alt="icon" />
+            <h3 className="mt-[24px] mb-[12px] text-2xl font-bold">Efficient <br></br>Shipping</h3>
+            <p className="text-base">
               We simplify Amazon product ordering with seamless transactions and
               reliable delivery
             </p>
           </li>
-          <li>
-            {/* <Image src="" alt="icon" /> */}
-            <h3>Customer Support</h3>
-            <p>
+          <li className="mr-[24px] pt-[20px] pb-[24px] px-[24px] rounded-[12px] bg-darkBlue text-captionalWhite text-center">
+          <Image src="/support-desktop-1x.png" width={234} height={234} alt="icon" />
+            <h3 className="mt-[24px] mb-[12px] text-2xl font-bold">Customer <br></br>Support</h3>
+            <p className="text-base">
               We simplify Amazon product ordering with seamless transactions and
               reliable delivery
             </p>
