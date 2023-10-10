cee29d8c58914e716b17a0e62dac49c9d7b59089
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-10-10 15:10:53 +0200

    implemented gtm into the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter, Red_Hat_Display } from "next/font/google";
+import Script from "next/script";
 
+const GTM_ID = "GTM-KB97P7BJ";
 const inter = Inter({ subsets: ["latin"] });
 const redHatDisplay = Red_Hat_Display({ subsets: ["latin"] });
 
@@ -12,7 +14,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html className="scroll-smooth" lang="en">
-      <body className={redHatDisplay.className}>{children}</body>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <body className={redHatDisplay.className}>
+        {children}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+      </body>
     </html>
   );
 }
