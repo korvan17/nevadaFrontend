275517fc528d9bef31bea5a30f5900437f0ec489
savelii <savvageljinskiy@gmail.com>
2023-10-21 23:51:22 +0200

    feat: contact info updt

--- src/components/Footer/FooterLinks/FooterContacs/FooterContacs.jsx
+++ src/components/Footer/FooterLinks/FooterContacs/FooterContacs.jsx
@@ -4,14 +4,14 @@ export default function FooterContacs() {
   return (
     <ul>
       <li className="mb-[8px]">
-        <a target="_blank" href="companyname@gmail.com">
+        <a target="_blank" href="info@ppcwarehouses.com" className="flex flex-wrap">
           <b className="font-semibold	">Write to us at the email address:</b>
-          companyname@gmail.com
+          info@ppcwarehouses.com
         </a>
       </li>
       <li>
-        <a href="tel:+48 000 000 000">
-          <b className="font-semibold">Phone:</b> +48 000 000 000
+        <a href="tel:+17027010078">
+          <b className="font-semibold">Phone:</b> +1 (702) 701-0078
         </a>
       </li>
     </ul>
