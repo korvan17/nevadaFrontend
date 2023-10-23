75649e0fdf6ba5699a1403c77d1e036af5a4a2fb
savelii <savvageljinskiy@gmail.com>
2023-10-23 04:00:26 +0200

    feat: started with grid

--- src/components/Footer/FooterLinks/FooterLinks.jsx
+++ src/components/Footer/FooterLinks/FooterLinks.jsx
@@ -7,21 +7,21 @@ import { FooterContacs } from "./FooterContacs";
 
 export default function FooterLinks() {
   return (
-    <div className="flex justify-between flex-wrap border-y border-white py-[48px]">
-      <div className="w-[282px]">
+    <div className="grid grid-cols-[164px] gap-x-[16px] gap-y-[24px] border-y border-white py-[48px] w-[375px] md:grid-cols-[164px] md:gap-x[20px]">
+      <div className="col-span-1 w-[282px]">
         <FooterCommonList data={links} />
       </div>
-      <div className="w-[282px]">
+      <div className="col-span-1 w-[282px] md:col-start-2 md:col-span-1">
         <h3 className="text-[18px] font-bold mb-[24px]">Help</h3>
         <FooterCommonList data={helpLinks} />
       </div>
-      <div className="w-[282px]">
+      <div className="col-span-2 w-[282px] md:col-start-3 md:col-span-1">
         <h3 className="text-[18px] font-bold mb-[24px]">
           Do you have a question?
         </h3>
-        <FooterContacs/>
+        <FooterContacs />
       </div>
-      <div className="w-[282px]">
+      <div className="col-span-2 w-[282px] ">
         <h3 className="text-[18px] font-bold mb-[24px]">Social networks</h3>
         <SocialLinks />
       </div>
