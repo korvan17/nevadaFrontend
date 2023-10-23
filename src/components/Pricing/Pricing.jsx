069a9de19188cb51e2b93815e1cb75bef82f1498
Yurii Soroka <yurisoroka165@gmail.com>
2023-10-23 15:37:04 +0300

    raw aditional servisec

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -153,7 +153,7 @@ export default function Pricing() {
 						</SwiperSlide>
 					</Swiper>
 				</div>
-				<ul className="hidden sm:hidden md:flex lg:flex flex-wrap">
+				<ul className="hidden sm:hidden md:flex gap-y-5 lg:flex flex-wrap mb-2">
 					<li className="w-full h-[316px] sm:w-full md:w-[282px] lg:w-[282px] mb-4 sm:mb-4 md:mb-0 py-4 pl-4 pr-9 rounded-lg bg-darkBlue text-captionalWhite overflow-x-auto md:mr-[20px] lg:mb-0 lg:mr-[24px]">
 						<h3 className="mt-[8px] mb-[12px] text-lg font-bold">
 							FBA, FBW, Private Label
@@ -263,6 +263,150 @@ export default function Pricing() {
 						<Buttons>Get a Quote</Buttons>
 					</li>
 				</ul>
+				<p className="sm:mb-4 text-xs text-captionalGreyLight lg:w-[741px] lg:text-base">
+					Base price* - Explanation: "At Prime Preparation Center, we
+					offer volume-based pricing, which means that the more you
+					purchase or the larger your order quantity, the lower the
+					unit price becomes. This approach rewards you for your
+					commitment to our services."
+				</p>
+				<div className="w-full h-[516px] sm:w-full md:w-[282px] h-[770px] lg:w-[1200px] h-[520px] mb-4 sm:mb-4 md:mb-0 rounded-lg bg-darkBlue text-captionalWhite overflow-x-auto md:mr-[20px] lg:mb-0 lg:mr-[24px] lg:py-[32px] px-[52px]">
+					<h3 className="mb-[12px] text-lg font-bold">
+						Additional services
+					</h3>
+
+					<table class="table-auto lg:w-[100%] lg:mb-[32px]">
+						<thead className="text-right">
+							<tr>
+								<th></th>
+								<th>Receiving</th>
+								<th>Inspection</th>
+								<th>Forwarding</th>
+								<th>Storage</th>
+								<th>Utilization</th>
+								<th>Removals</th>
+								<th>Pictures 3pcs</th>
+							</tr>
+						</thead>
+						<tbody className="text-right leading-[200%]">
+							<tr>
+								<td className="text-left">
+									Box (24x18x12) 49 lbs max
+								</td>
+								<td>$1</td>
+								<td>$1</td>
+								<td>$5</td>
+								<td>$5</td>
+								<td>$2</td>
+								<td>$2</td>
+								<td>$1</td>
+							</tr>
+							<tr>
+								<td className="text-left">Pallet (48x40x72)</td>
+								<td>$25</td>
+								<td>$1</td>
+								<td>$25</td>
+								<td>$35</td>
+								<td>$20</td>
+								<td>$20</td>
+								<td>$1</td>
+							</tr>
+							<tr>
+								<td className="text-left">
+									PL Container (20ft)
+								</td>
+								<td>$300</td>
+								<td>$100</td>
+								<td>-</td>
+								<td>-</td>
+								<td>-</td>
+								<td>-</td>
+								<td>-</td>
+							</tr>
+							<tr>
+								<td className="text-left">
+									FL Container (40ft)
+								</td>
+								<td>$600</td>
+								<td>$150</td>
+								<td>-</td>
+								<td>-</td>
+								<td>-</td>
+								<td>-</td>
+								<td>-</td>
+							</tr>
+						</tbody>
+					</table>
+
+					<div className="lg: flex justify-between items-center">
+						<p className="">HazMat, Fragile, Oversize</p>
+						<p className="lg: w-[866px] text-base">
+							Hazmat items incur additional costs due to strict
+							safety regulations and specialized handing
+							requirements. Fragile items come with extra fees to
+							ensure meticulous handling and the use of protective
+							materials to prevent damage during transit.
+							Oversized items result in higher charges because
+							they demand more space and specialized equipment for
+							secure transportation and storage
+						</p>
+					</div>
+					<h3 className="mt-[8px] mb-[12px] text-lg font-bold">
+						Packing services
+					</h3>
+					<ul className="lg:flex justify-between">
+						<li className="lg:w-[202px]">
+							<table className="lg:w-[100%]">
+								<tr>
+									<td>Bubble bag/wrap</td>
+									<td>$0,35</td>
+								</tr>
+								<tr>
+									<td>Polybag</td>
+									<td>$0,30</td>
+								</tr>
+							</table>
+						</li>
+						<li className="lg:w-[136px]">
+							<table className="lg:w-[100%] ">
+								<tr>
+									<td className="text-left">Palletizing</td>
+									<td className="text-right">$25</td>
+								</tr>
+								<tr>
+									<td className="text-left">Box 10x8x6</td>
+									<td className="text-right">$1</td>
+								</tr>
+							</table>
+						</li>
+						<li className="lg:w-[138px]">
+							<table className="lg:w-[100%]">
+								<tr>
+									<td className="text-left">Box 10x10x10</td>
+									<td className="text-right">$2</td>
+								</tr>
+								<tr>
+									<td className="text-left">Box 24x14x6</td>
+									<td className="text-right">$3</td>
+								</tr>
+							</table>
+						</li>
+						<li className="lg:w-[218px]">
+							<table className="lg:w-[100%]">
+								<tr>
+									<td className="text-left">Box 24x18x12</td>
+									<td className="text-right">$4</td>
+								</tr>
+								<tr>
+									<td className="text-left">
+										Additional (min 1 hour)
+									</td>
+									<td className="text-right">$40</td>
+								</tr>
+							</table>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</section>
 	);
