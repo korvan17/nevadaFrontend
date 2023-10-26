3fc19c74a236225111d58521fc737bec6ab1d51a
PilyaP <ppaexpert@gmail.com>
2023-10-27 00:16:05 +0300

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-/* global module */
 module.exports = {
   env: {
     browser: true,
--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const nextConfig = {
     unoptimized: true,
   },
 };
-/* global module */
+
 
 module.exports = nextConfig;
