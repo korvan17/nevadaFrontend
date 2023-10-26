315b2f974dc8992a83d357d1b5c33b3fd84fabcf
PilyaP <ppaexpert@gmail.com>
2023-10-27 02:37:32 +0300

    contacts form modal mobile done

--- src/components/GetQueteButton/GetQuoteButton.jsx
+++ src/components/GetQueteButton/GetQuoteButton.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import React, { useState } from "react";
 import BasicModal from "../Modals/BasicModal/BasicModal";
 import GetQuoteWidgetModal from "../Modals/GetQuoteWidgetModal/GetQuoteWidgetModal";
@@ -19,7 +18,7 @@ export default function GetQuoteButton() {
     <>
       <button
         onClick={toggleGetQuoteModal}
-        className="fixed shadowButton rounded-[50px] w-[72px] h-[72px] leading-[16px] font-bold text-[16px] text: bg-accentYellow text-[#FAFCF8] hover:bg-accentHoverYellow right-[120px] top-[702px]"
+        className="fixed shadowButton rounded-[50px] w-[72px] h-[72px] leading-[16px] font-bold text-[16px] text: bg-accentYellow text-[#FAFCF8] hover:bg-accentHoverYellow right-[48px] top-[702px]"
       >
         GET A QUOTE
       </button>
--- src/components/Modals/GetQuoteWidgetModal/GetQuoteWidgetModal.jsx
+++ src/components/Modals/GetQuoteWidgetModal/GetQuoteWidgetModal.jsx
@@ -3,8 +3,105 @@ import React from "react";
 export default function GetQuoteWidgetModal() {
   return (
     <div>
-      <h2>Contacts Form</h2>
-      <select></select>
+      <h2 className="text-center text-[24px] font-semibold mt-6 text-[#000A11]">
+        Contacts Form
+      </h2>
+      <div className="mb-4">
+        <label
+          htmlFor="Business direction"
+          className="block mt-6 ml-[22px] font-medium"
+        >
+          Business direction
+        </label>
+
+        <select
+          id="Select Your Business"
+          className=" p-2 border rounded w-[280px] h-[60px] ml-[22px]"
+        >
+          <option>Select Your Business</option>
+          <option>FBA, FBW, Private Label</option>
+          <option>Online arbitration</option>
+          <option>FBM</option>
+          <option>Wholesale</option>
+        </select>
+      </div>
+
+      <div className="mt-[118px]">
+        <label
+          htmlFor="fullName"
+          className="block mb-3 font-semibold ml-2 leading-[24px] text-[#000A11]"
+        >
+          Full Name <span className="text-red-500">*</span>
+        </label>
+        <input
+          type="text"
+          id="fullName"
+          className="w-[327px] p-4 border rounded ml-1"
+          required
+        />
+      </div>
+
+      <div className="mt-4">
+        <label
+          htmlFor="companyName"
+          className="block mb-3 font-semibold ml-2 leading-[24px] text-[#000A11]"
+        >
+          Company Name
+        </label>
+        <input
+          type="text"
+          id="companyName"
+          className="w-[327px] p-4 border rounded ml-1"
+        />
+      </div>
+
+      <div className="mt-4">
+        <label
+          htmlFor="email"
+          className="block mb-3 font-semibold ml-2 leading-[24px] text-[#000A11]"
+        >
+          Email Address <span className="text-red-500">*</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="w-[327px] p-4 border rounded ml-1"
+          required
+        />
+      </div>
+
+      <div className="mt-4">
+        <label
+          htmlFor="website"
+          className="block mb-3 font-semibold ml-2 leading-[24px] text-[#000A11]"
+        >
+          Company Website
+        </label>
+        <input
+          type="url"
+          id="website"
+          className="w-[327px] p-4 border rounded ml-1"
+        />
+      </div>
+
+      <div className="mt-4">
+        <label
+          htmlFor="message"
+          className="block mb-3 font-semibold ml-2 leading-[24px] text-[#000A11]"
+        >
+          Comments
+        </label>
+        <textarea
+          id="comments"
+          rows="5"
+          className="w-[327px] p-4 border rounded ml-1 resize-none"
+        ></textarea>
+      </div>
+      <div className="flex justify-center">
+        <button className="bg-accentYellow text-white px-4 py-2 rounded hover:bg-accentHoverYellow ml-[auto] mr-[auto] font-bold text-[16px] w-[179px] h-[48px]">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
