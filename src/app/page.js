a0e9b609931a884e6aaf933acf9c6902d897bc41
PilyaP <ppaexpert@gmail.com>
2023-10-26 03:13:45 +0300

    testfix

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
-      <Services />
+      {/* <Services /> */}
       {/* <Pricing /> */}
       <Faq />
       <Footer />
