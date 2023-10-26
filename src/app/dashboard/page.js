27052e290727e5c367913f78a1c9a418c5498600
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-10-26 14:36:17 +0300

    add dashboard

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "PPC | Dashboard",
+  description: "Dashboard client page",
+};
+
+function Dashboard() {
+  return (
+    <>
+      <h1 className="text-[50px] w-[450px] mt-[120px] mx-auto">
+        Dashboard Page
+      </h1>
+    </>
+  );
+}
+
+export default Dashboard;
