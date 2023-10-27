5364b9e2dbed1a4ec8359abe4dbcc8734d0c4e01
savelii <savvageljinskiy@gmail.com>
2023-10-28 00:45:18 +0200

    ref: detected solution without 'use client'

--- src/components/Footer/FooterLinks/FooterLinks.jsx
+++ src/components/Footer/FooterLinks/FooterLinks.jsx
@@ -1,45 +1,16 @@
-"use client";
-
 import React from "react";
 
 import { SocialLinks } from "./SocialLinks";
 import { FooterContacs } from "./FooterContacs";
-import { useEffect, useState } from "react";
 import { FooterLinksLeftSide } from "./FooterLinksLeftSide";
 
 export default function FooterLinks() {
-  const [viewportWidth, setViewportWidth] = useState(
-    typeof window !== "undefined" ? window.innerWidth : 0
-  );
-
-  useEffect(() => {
-    const handleResize = () => {
-      setViewportWidth(window.innerWidth);
-    };
-
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", handleResize);
-    }
-
-    return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("resize", handleResize);
-      }
-    };
-  }, []);
-
-  const mobileViewPort = viewportWidth < 768;
+  const t = true;
   return (
     <div className="border-y border-white container">
-      <div className="flex md:flex-row flex-col flex-wrap py-[48px] justify-between md:w-[564px] lg:w-auto mx-auto">
-        {mobileViewPort ? (
-          <div className="flex justify-between w-full">
-            <FooterLinksLeftSide />
-          </div>
-        ) : (
-          <FooterLinksLeftSide />
-        )}
-        <div className="mb-[24px]">
+      <div className="flex flex-row  flex-wrap py-[48px] justify-between md:w-[564px] lg:w-auto mx-auto">
+        <FooterLinksLeftSide />
+        <div className="mb-[24px] w-full md:w-auto">
           <h3 className="text-[18px] font-bold mb-[24px]">
             Do you have a question?
           </h3>
