d7c54bc25425d5d81923eb7cd4ed0153adb8ddad
savelii <savvageljinskiy@gmail.com>
2023-10-28 03:55:11 +0200

    fixed lint errors

--- src/components/Footer/FooterLinks/FooterLinks.jsx
+++ src/components/Footer/FooterLinks/FooterLinks.jsx
@@ -8,24 +8,26 @@ import { helpLinks } from "./footer-data";
 
 export default function FooterLinks() {
   return (
-    <div className="border-y border-white container">
-      <div className="flex flex-row  flex-wrap justify-between md:w-[564px] lg:w-auto mx-auto py-[24px] lg:py-[48px] text-[14px]">
-        <div className="mb-[24px]">
-          <FooterCommonList data={links} />
-        </div>
-        <div className="mb-[24px]">
-          <h3 className="text-[18px] font-bold mb-[24px]">Help</h3>
-          <FooterCommonList data={helpLinks} />
-        </div>
-        <div className="mb-[24px] w-full md:w-auto">
-          <h3 className="text-[18px] font-bold mb-[24px]">
-            Do you have a question?
-          </h3>
-          <FooterContacs />
-        </div>
-        <div>
-          <h3 className="text-[18px] font-bold mb-[24px]">Social networks</h3>
-          <SocialLinks />
+    <div className="container">
+      <div className="border-y border-white">
+        <div className="flex flex-row flex-wrap justify-between md:w-[564px] lg:w-auto mx-auto py-[24px] lg:pt-[49px] lg:pb-[47px] text-[14px]">
+          <div className="mb-[24px]">
+            <FooterCommonList data={links} />
+          </div>
+          <div className="mb-[24px]">
+            <h3 className="text-[18px] font-bold mb-[24px]">Help</h3>
+            <FooterCommonList data={helpLinks} />
+          </div>
+          <div className="mb-[24px] w-full md:w-auto">
+            <h3 className="text-[18px] font-bold mb-[24px]">
+              Do you have a question?
+            </h3>
+            <FooterContacs />
+          </div>
+          <div>
+            <h3 className="text-[18px] font-bold mb-[24px]">Social networks</h3>
+            <SocialLinks />
+          </div>
         </div>
       </div>
     </div>
