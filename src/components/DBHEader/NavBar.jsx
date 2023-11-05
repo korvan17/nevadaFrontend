8ebc9059aea9272d26a05465064bdf911883e368
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-05 20:50:47 +0200

    add list in nav

--- src/components/DBHEader/NavBar.jsx
+++ src/components/DBHEader/NavBar.jsx
@@ -2,23 +2,35 @@ import React from "react";
 import { HomeIcon, UserIcon } from "../Icons";
 import Link from "next/link";
 
+const navLinks = [
+  {
+    icon: <UserIcon />,
+    title: "User page",
+    href: "/dashboard/userPage",
+  },
+  {
+    icon: <HomeIcon />,
+    title: "Home page",
+    href: "/dashboard",
+  },
+];
+
 function NavBar() {
   return (
-    <nav className="flex gap-[16px] justify-center">
-      <Link
-        href="/dashboard/userPage"
-        className="flex items-center justify-center w-[48px] h-[48px]"
-        title="User page"
-      >
-        <UserIcon />
-      </Link>
-      <Link
-        href="/dashboard"
-        className="flex items-center justify-center w-[48px] h-[48px]"
-        title="Home page"
-      >
-        <HomeIcon />
-      </Link>
+    <nav className="">
+      <ul className="flex gap-[16px] justify-center">
+        {navLinks.map(({ icon, title, href }) => (
+          <li key={title}>
+            <Link
+              href={href}
+              className="flex items-center justify-center w-[48px] h-[48px]"
+              title={title}
+            >
+              {icon}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
