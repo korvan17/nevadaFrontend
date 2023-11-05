18e7e379b8e69610cce167338d5a44933f801cdb
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-05 19:50:28 +0200

    change home header styles

--- src/components/Header/DesktopHeader.jsx
+++ src/components/Header/DesktopHeader.jsx
@@ -5,12 +5,26 @@ import React from "react";
 function DesktopHeader() {
   return (
     <div
-      className="hidden lg:block fixed top-0 z-20 bg-mainBlack text-mainWhite
-     text-sm[16px] w-[100%] pt-[39px] pb-[8px]  mg:py-[11px] lg:pt-[46px] lg:pb-[16px]"
+      className="hidden 
+      fixed top-0 z-20 bg-mainBlack text-mainWhite
+      text-sm[16px] w-[100%]  pb-[8px]
+      lg:block lg:pb-[16px] pt-[76px] 
+      mg:py-[11px]"
     >
-      <div className="gap-[20px] text-[16px] font-semibold	 w-[100%] absolute top-0 h-[30px] hidden lg:flex items-center justify-center bg-captionBlue text-captionalWhite">
-        <a href="mailto:info@ppcwarehouses.com">info@ppcwarehouses.com</a>
-        <a href="tel:+1 (702) 701-0078">+1 (702) 701-0078</a>
+      <div
+        className="
+        w-[100%] absolute top-0 h-[56px] 
+        flex items-center justify-center flex-col
+         bg-captionBlue text-captionalWhite
+        text-[16px] font-semibold 
+        "
+      >
+        <a className="" href="mailto:info@ppcwarehouses.com">
+          info@ppcwarehouses.com
+        </a>
+        <a className="" href="tel:+1 (702) 701-0078">
+          +1 (702) 701-0078
+        </a>
         {/* <Link
           className="rounded-[8px] bg-accentYellow text-[#FAFCF8] px-1"
           href="/dashboard"
