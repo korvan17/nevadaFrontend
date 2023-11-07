0ea82566faf37ab59176e7cdedf73a30dd5d7909
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-07 20:49:00 +0200

    DB desktop header styles

--- src/components/DBHEader/DBHeader.jsx
+++ src/components/DBHEader/DBHeader.jsx
@@ -23,14 +23,24 @@ function DBHeader() {
           <DarkLogo />
           Prime Preparation Center
         </a>
-        <div className="flex items-center justify-center">
-          <p className="hidden md:block md:leading-[16px] font-medium	text-[16px] mr-[24px]">
+        <div className="flex items-center justify-center lg:gap-[24px]">
+          <p className="hidden md:block md:leading-[16px] font-medium	text-[16px] md:mr-[24px]">
             Main
           </p>
-          <div className="hidden md:block mr-[37px]">
+          <div className="hidden md:block md:mr-[37px]">
             <LanguageSwitcher page="db" />
           </div>
-          <NavBar />
+          <button
+            className="hidden lg:block
+         bg-captionBlue text-mainWhite text-[16px] font-bold  leading-[16px]
+         w-[179px] h-[48px] rounded-[8px]"
+            type="button"
+          >
+            Logout
+          </button>
+          <div className="lg:hidden">
+            <NavBar />
+          </div>
         </div>
       </div>
     </header>
