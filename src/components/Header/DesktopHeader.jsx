2b1ae0567eeca15d71cfb0a33292c7e11ee18ef8
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-07 21:09:16 +0200

    hide UC

--- src/components/Header/DesktopHeader.jsx
+++ src/components/Header/DesktopHeader.jsx
@@ -35,7 +35,7 @@ function DesktopHeader() {
       <div className="container">
         <div className="flex">
           <Navigation />
-          <UserControls />
+          {/* <UserControls /> */}
         </div>
       </div>
     </div>
