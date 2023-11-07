726183e6fed94f6149d6124336ce283cf044adb7
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-07 19:51:29 +0200

    toggle icons + DB pages

--- src/components/DBHEader/NavBar.jsx
+++ src/components/DBHEader/NavBar.jsx
@@ -20,9 +20,13 @@ const navLinks = [
 
 function NavBar() {
   const pathName = usePathname();
+
+  const homePage = pathName === "/dashboard";
+  const userPage = pathName === "/dashboard/account";
+
   return (
     <nav className="">
-      <ul className="flex gap-[16px] justify-center">
+      <ul className="flex gap-[16px] justify-center md:hidden">
         {navLinks.map(({ icon, title, href }) => {
           const isActive = pathName === href;
           return (
@@ -44,6 +48,17 @@ function NavBar() {
           );
         })}
       </ul>
+      <div className="hidden md:flex gap-[16px] justify-center">
+        <Link href={userPage ? "/dashboard" : "/dashboard/account"}>
+          <button
+            type="button"
+            className="bg-captionBlue text-captionalWhite
+             flex items-center justify-center w-[48px] h-[48px] rounded-full"
+          >
+            {homePage ? <UserIcon /> : <HomeIcon />}
+          </button>
+        </Link>
+      </div>
     </nav>
   );
 }
