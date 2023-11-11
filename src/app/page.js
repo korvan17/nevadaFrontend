6321ebd22cda394e2b2b6655f7441e665cb6775a
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-11-11 20:30:19 +0100

    fixed error with animation

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ import {
 import GetQuoteButton from "@/components/UIElements/GetQueteButton/GetQuoteButton";
 import { useEffect } from "react";
 import AOS from "aos";
+import 'aos/dist/aos.css';
 
 export default function Home() {
   useEffect(() => {
--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
+
 export default function Hero() {
   return (
     <section
