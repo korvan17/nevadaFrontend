2ceb6cae2124f5aa46a0efa0b81e32f7bed50a63
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-11 18:15:31 +0200

    move about to Home folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as About } from "./About/About.jsx";
+export { default as About } from "./Home/About/About.jsx";
 export { default as Header } from "./Header/Header.jsx";
 export { default as Faq } from "./Faq/Faq.jsx";
 export { default as Services } from "./Services/Services.jsx";
