19d974e1f121146a99f5abd485d417c9f70848b5
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-11 19:52:05 +0200

    add UserPanel

--- src/components/Dashboard/DBContentContainer/DBContentContainer.jsx
+++ src/components/Dashboard/DBContentContainer/DBContentContainer.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import UserPanel from "../UserPanel/UserPanel";
 
 function DBContentContainer() {
   return (
-    <div
-      className="container mt-[148px] h-full bg-bgBoard rounded-[16px]
-    custom-shadow"
-    >
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aut atque
-      natus earum laboriosam maxime quod! Inventore odit, nemo assumenda
-      accusamus consequuntur illum quod sunt nisi. Repudiandae nostrum atque
-      autem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aut
-      atque natus earum laboriosam maxime quod! Inventore odit, nemo assumenda
-      accusamus consequuntur illum quod sunt nisi. Repudiandae nostrum atque
-      autem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aut
-      atque natus earum laboriosam maxime quod! Inventore odit, nemo assumenda
-      accusamus consequuntur illum quod sunt nisi. Repudiandae nostrum atque
-      autem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aut
-      atque natus earum laboriosam maxime quod! Inventore odit, nemo assumenda
-      accusamus consequuntur illum quod sunt nisi. Repudiandae nostrum atque
-      autem.
+    <div className="container flex gap-[12px]">
+      <div
+        className="mt-[148px] h-full bg-bgBoard rounded-[16px]
+    custom-shadow p-[8px]"
+      >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis aut
+        atque natus earum laboriosam maxime quod! Inventore odit, nemo assumenda
+        accusamus consequuntur illum quod sunt nisi. Repudiandae nostrum atque
+        autem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+        aut atque natus earum laboriosam maxime quod! Inventore odit, nemo
+        assumenda accusamus consequuntur illum quod sunt nisi. Repudiandae
+        nostrum atque autem. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Omnis aut atque natus earum laboriosam maxime quod! Inventore
+        odit, nemo assumenda accusamus consequuntur illum quod sunt nisi.
+        Repudiandae nostrum atque autem. Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Omnis aut atque natus earum laboriosam maxime quod!
+        Inventore odit, nemo assumenda accusamus consequuntur illum quod sunt
+        nisi. Repudiandae nostrum atque autem.
+      </div>
     </div>
   );
 }
