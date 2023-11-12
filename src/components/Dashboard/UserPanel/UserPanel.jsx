91f68569e16616a9e27e692fca190accc120fdda
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-12 14:57:37 +0200

    add userPanelItems  + basic html to UserPanel component

--- src/components/Dashboard/UserPanel/UserPanel.jsx
+++ src/components/Dashboard/UserPanel/UserPanel.jsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
 import React from "react";
 
 function UserPanel() {
-  return <div className="">UserPanel</div>;
+  return (
+    <div className="container">
+      <Image href="https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+      <p>persomal data</p>
+      <p className="text-red">Discount</p>
+      <button type="button">Subcrabation</button>
+      <ul>
+        {userPanelItems.map(({ title, icon, hint }) => {
+          return (
+            <li>
+              <p>{title}</p>
+              <p>{hint}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default UserPanel;
---
+++ src/content/userPanelItems.js
@@ -0,0 +1,24 @@
+const userPanelItems = [
+  {
+    title: "Password settings",
+    icon: "icon svg component",
+    hint: "Change password",
+  },
+  {
+    title: "History of discounts",
+    icon: "icon svg component",
+    hint: "See all discounts",
+  },
+  {
+    title: "Transaction history",
+    icon: "icon svg component",
+    hint: "See all transaction",
+  },
+  {
+    title: "Change the language",
+    icon: "icon svg component",
+    hint: "Change the interface language",
+  },
+];
+
+export default userPanelItems;
