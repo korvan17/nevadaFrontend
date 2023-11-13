f39b4268d908a9444fee4a9bac601162d048079f
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-11-13 17:16:32 +0100

    Update Calendar.jsx

--- src/components/UIElements/Datepicker/Calendar.jsx
+++ src/components/UIElements/Datepicker/Calendar.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
