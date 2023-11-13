9d7424fe65b455f77f99abdc7eeefc200517128a
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2023-11-13 20:31:30 +0200

    remove extra lib

--- src/content/passwordUpdateData.js
+++ src/content/passwordUpdateData.js
@@ -1,17 +1,14 @@
 const passwordUpdateData = [
   {
     label: "Enter your current password",
-    inputValue: "",
     name: "current",
   },
   {
     label: "Enter a new password",
-    inputValue: "",
     name: "new",
   },
   {
     label: "Repeat the new password",
-    inputValue: "",
     name: "repeat",
   },
 ];
