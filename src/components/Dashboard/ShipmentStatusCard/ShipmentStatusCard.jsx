d9532938a6d29ab8ba8b3eb53dc208b73ecf4ec2
PilyaP <ppaexpert@gmail.com>
2024-01-20 22:11:06 +0200

    cards link

--- src/components/Dashboard/ShipmentStatusCard/ShipmentStatusCard.jsx
+++ src/components/Dashboard/ShipmentStatusCard/ShipmentStatusCard.jsx
@@ -1,15 +1,17 @@
 const ShipmentStatusCard = ({ title, icon, count, children }) => (
-  <div className="p-5 w-72 h-38 rounded-md border border-gray-300">
-    <div>
-      <h3 className="text-lg font-semibold mb-3">{title}</h3>
-      {children}
+  <a href="/dashboard/shipments">
+    <div className="p-5 w-72 h-38 rounded-md border border-gray-300">
+      <div>
+        <h3 className="text-lg font-semibold mb-3">{title}</h3>
+        {children}
+      </div>
+      <div className="flex items-center justify-between">
+        {icon}
+        <span className="text-2xl font-bold">{count}</span>
+      </div>
+      <div className="flex justify-end"></div>
     </div>
-    <div className="flex items-center justify-between">
-      {icon}
-      <span className="text-2xl font-bold">{count}</span>
-    </div>
-    <div className="flex justify-end"></div>
-  </div>
+  </a>
 );
 
 export default ShipmentStatusCard;
