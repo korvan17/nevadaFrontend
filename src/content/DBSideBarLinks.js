6498314517f4d8ccbed238bf2eb87900e0c6643e
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2024-01-22 14:01:22 +0200

    add sidebar hints (titles)

--- src/content/DBSideBarLinks.js
+++ src/content/DBSideBarLinks.js
@@ -9,18 +9,22 @@ const DBSideBarLinks = [
   {
     href: "/dashboard/account",
     icon: <UserIcon />,
+    title: "Account",
   },
   {
     href: "/dashboard",
     icon: <ShipmentsIcon />,
+    title: "Dashboard home page",
   },
   {
     href: "/dashboard/messages",
     icon: <MessagesIcon />,
+    title: "Messages",
   },
   {
     href: "/dashboard/shipments",
     icon: <InboundShipmentsIcon />,
+    title: "Shipments",
   },
 ];
 
