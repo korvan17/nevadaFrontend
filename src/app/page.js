637a84ab67858dc828833ac0038a21042412ef16
PilyaP <ppaexpert@gmail.com>
2024-01-31 23:39:24 +0200

    fix button

--- src/app/page.js
+++ src/app/page.js
@@ -36,9 +36,6 @@ export default function Home() {
       <Faq />
       <GetQuoteButton />
       <TelegramButton />
-      {/* <ChatWindow /> */}
-      {/* <div id="myportal" /> */}
-
       <ModalRender />
     </>
   );
