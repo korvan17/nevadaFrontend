4355a26c69c12f3ddfb2c07622a28960e167a5fd
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2024-02-02 01:37:42 +0200

    fix filter error

--- src/components/Dashboard/Shipments/ShipmentsPagination.jsx
+++ src/components/Dashboard/Shipments/ShipmentsPagination.jsx
@@ -6,27 +6,29 @@ import ReactPaginate from "react-paginate";
 function ShipmentsPagination({ pageCount, handlePageClick, currentPage }) {
   return (
     <ReactPaginate
-      previousLabel={<KeyboardArrowLeft />}
-      nextLabel={<KeyboardArrowRight />}
+      previousLabel={<KeyboardArrowLeft fontSize="small" />}
+      nextLabel={<KeyboardArrowRight fontSize="small" />}
       pageCount={pageCount}
       onPageChange={handlePageClick}
-      containerClassName={"flex list-none justify-center gap-[10px] my-[25px]"}
+      containerClassName={
+        "flex list-none justify-center gap-[5px] my-[10px] my-[25px] "
+      }
       forcePage={currentPage}
       pageRangeDisplayed={2}
       marginPagesDisplayed={1}
       breakLabel={"..."}
       pageLinkClassName={
-        "border px-4 py-2 mx-1 mb-2 sm:mb-0 sm:mx-2 transition duration-300 ease-in-out hover:bg-captionalGrey"
+        "border px-4 py-2 mx-1 mb-2 sm:mb-0 sm:mx-2 transition duration-300 ease-in-out hover:bg-captionalGrey rounded-[8px]"
       }
       activeLinkClassName={
-        "bg-captionBlueDB text-white border-captionBlueDB hover:bg-captionBlueDB radius-[20px]"
+        "bg-captionBlueDB text-white border-captionBlueDB hover:bg-captionBlueDB rounded-[20px]"
       }
-      breakLinkClassName={"mx-2"}
+      breakLinkClassName={"mx-1"}
       previousLinkClassName={
-        "border px-4 py-2 mr-2 transition duration-300 ease-in-out hover:bg-captionalGrey"
+        "border px-2 py-1 mr-1 transition duration-300 ease-in-out hover:bg-captionalGrey rounded-[4px]"
       }
       nextLinkClassName={
-        "border px-4 py-2 ml-2 transition duration-300 ease-in-out hover:bg-captionalGrey"
+        "border px-2 py-1 ml-1 transition duration-300 ease-in-out hover:bg-captionalGrey rounded-[4px]"
       }
       disabledClassName={"text-dbActiveLink cursor-not-allowed"}
     />
